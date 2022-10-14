2fad416b20392667872ff560be33a2f61b1f317f
Matthew <matthew.schafer@icahn.mssm.edu>
2022-10-14 16:04:06 -0400

    fixing install

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,13 +1,26 @@
 # to make importing easier
-from .circ_stats import *
-from .classification import *
-from .dim_reduction import *
-from .func_conn import *
-from .io import *
-from .masking import *
-from .matrices import *
-from .mvpa import *
-from .plotting import *
-from .regression import *
-from .stats import *
-from .utils import *
+import .circ_stats
+import .classification
+import .dim_reduction 
+import .func_conn 
+import .io 
+import .masking 
+import .matrices
+import .mvpa 
+import .plotting 
+import .regression 
+import .stats
+import .utils 
+
+# from .circ_stats import *
+# from .classification import *
+# from .dim_reduction import *
+# from .func_conn import *
+# from .io import *
+# from .masking import *
+# from .matrices import *
+# from .mvpa import *
+# from .plotting import *
+# from .regression import *
+# from .stats import *
+# from .utils import *
